402790fe23fb876ded6db8e73adcc984737c1d21
EzeScigo <scigolini@gmail.com>
2021-04-14 16:42:39 +0200

    debounce hook

--- src/hooks.js
+++ src/hooks.js
@@ -14,8 +14,23 @@ export const useOnClickOutside = (ref, handler) => {
       document.removeEventListener('mousedown', listener);
     };
   },
-  [ref, handler],
+    [ref, handler],
   );
+};
+
+export function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 };
 
 
